19571e73ddb933595d0b6f716aaac6200084aa20
Joseph Juma <10J@users.noreply.github.com>
2016-08-11 11:19:36 -0700

    Updated AI.java to give more structure
    I updated this file to give it a classic "bot" structure that is used in most game AI. This should let it become easier to make the AI smarter and able to be better tweaked or derived from without rewriting single "master" methods for all of their behavior.

--- core/src/com/shadyaardvark/players/AI.java
+++ core/src/com/shadyaardvark/players/AI.java
@@ -1,46 +1,53 @@
-package com.shadyaardvark.players;
-
 import com.badlogic.gdx.utils.Array;
 import com.shadyaardvark.map.GameBoard;
 import com.shadyaardvark.map.Region;
 
 public class AI implements Player {
-    private final int team;
-
-    public AI(int team) {
-        this.team = team;
-    }
-
-    @Override
-    public int getTeam() {
-        return team;
-    }
-
-    @Override
-    public void doTurn(GameBoard board) {
-        for (Region region : board.getTeamRegions(team)) {
-            if (region.getDice() != 1) {
-                attack(region, board);
-            }
-        }
-
-        board.endTurn();
-    }
-
-    private void attack(Region region, GameBoard board) {
-        if (region.getDice() == 1) {
-            return;
-        }
-
-        Array<Region> neighbors = region.getNeighboringRegions();
-        neighbors.sort();
-
-        for (Region neighbor : neighbors) {
-            if (neighbor.getTeam() != team && region.getDice() >= neighbor.getDice()) {
-                if (board.attack(region, neighbor)) {
-                    attack(neighbor, board);
-                }
-            }
-        }
-    }
+	
+	private final int team;
+	
+	
+	public AI(int team){
+	}
+	
+	@Override
+	public int getTeam(){
+		return team;
+	}
+	
+	@Override
+	public void doTurn(GameBoard board){
+		sense();
+	}
+	
+	public void sense(GameBoard board){
+			for(Region region : board.getTeamRegions(team)){
+			if(region.getDice() != 1){
+				think(region,board);
+			}
+		}
+		board.endTurn();
+	}
+	
+	public void think(Region region, GameBoard board){
+		if(region.getDice() == 1){
+			return;
+		}
+		
+		Array<Region> neighbors = region.getNeighboringRegions();
+		neighbors.sort();
+		
+		for(Region neighbor : neighbors){
+			if(neighbor.getTeam() != team && region.getDice() >= neighbor.getDice()){
+				act(board,region,neighbor);
+			}
+		}
+	}
+	
+	public void act(GameBoard board, Region region, Region neighbor){
+		if(board.attack(region,neighbor)){
+			think(neighbor,board);
+		}
+	}
+	
 }
