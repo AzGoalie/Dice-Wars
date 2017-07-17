a6fc2ca858692b3fc666e43a40717b7aeedf4c00
Travis McMahon <azgoalie@gmail.com>
2017-07-17 00:36:33 -0700

    fixed hexagon generation

--- core/src/com/trashcob/dicewars/map/RegionGenerator.kt
+++ core/src/com/trashcob/dicewars/map/RegionGenerator.kt
@@ -5,7 +5,8 @@ import com.badlogic.gdx.utils.Array
 import com.trashcob.dicewars.map.layouts.GameBoardLayout
 import com.trashcob.dicewars.math.geom.hexagon.Hexagon
 
-class RegionGenerator(val width: Int, val height: Int, val layout: GameBoardLayout) {
+class RegionGenerator(val width: Int, val height: Int, layout: GameBoardLayout) {
+    private val availableHexagons: List<Hexagon> = layout.generateLayout(width, height)
 
     fun generateRegions(): List<Region> {
         val hexagons = generateHexagons() as MutableList<Hexagon>
@@ -18,11 +19,10 @@ class RegionGenerator(val width: Int, val height: Int, val layout: GameBoardLayo
     }
 
     private fun generateHexagons(): List<Hexagon> {
-        val availableHexagons: List<Hexagon> = layout.generateLayout(width, height)
         val hexagons = Array<Hexagon>()
 
         hexagons.add(availableHexagons[MathUtils.random(0, availableHexagons.size - 1)])
-        while (hexagons.size < width * height * 1) {
+        while (hexagons.size < width * height * 0.6) {
             hexagons.add(findNextAvailableHexagon(hexagons))
         }
 
@@ -30,9 +30,13 @@ class RegionGenerator(val width: Int, val height: Int, val layout: GameBoardLayo
     }
 
     private fun findNextAvailableHexagon(hexagons: Array<Hexagon>): Hexagon {
-        val hexagon = hexagons.random()
-        val next = hexagon.neighbor(MathUtils.random(0, Hexagon.directions.size - 1))
-        if (hexagons.contains(next)) findNextAvailableHexagon(hexagons)
+        var hexagon: Hexagon
+        var next: Hexagon
+
+        do {
+            hexagon = hexagons.random()
+            next = hexagon.neighbor(MathUtils.random(0, Hexagon.directions.size - 1))
+        } while (!availableHexagons.contains(next) || hexagons.contains(next))
 
         return next
     }
